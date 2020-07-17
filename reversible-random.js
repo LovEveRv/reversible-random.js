f897a5d83786b20b157aa2305d5cca675485178e
LovEver_Rv <yongweili.cn@gmail.com>
2020-07-17 15:59:58 +0800

    update comments

--- reversible-random.js
+++ reversible-random.js
@@ -1,3 +1,17 @@
+// reversible-random.js
+// ----------
+// This JS module provides the class 'ReversibleRandom',
+// which is a "reversible" PRNG with the ability to generate the previous random number.
+// That is, you can use 'next' to move to the next state, and use 'prev' to move to the previous state.
+// ----------
+// This PRNG works with the Linear Congruence Algorithm.
+// You are free to create a RNG with your own arguments (a, c, m and inv_a).
+// If you did not provide inv_a, it will be calculated automatically.
+// ----------
+// As for detailed usage and more detailed description, please see README.md,
+// or visit: https://github.com/LovEveRv/reversible-random.js
+
+
 /*
 ==============
 Util functions
@@ -6,7 +20,7 @@ Util functions
 
 // Extended Euclidean Algorithm.
 // Note:
-// this function uses the type 'BigInt'
+// this function uses the type 'BigInt',
 // which may not be supported in early JS versions.
 function exgcd (a, b, vec2) {
   if (b == 0) {
@@ -24,7 +38,7 @@ function exgcd (a, b, vec2) {
 // Find the inverses of a (mod n).
 // That is, find x s.t. a * x % n = 1.
 // Note:
-// this function uses the type 'BigInt'
+// this function uses the type 'BigInt',
 // which may not be supported in early JS versions.
 function getInverses (a, n) {
   var vec2 = {
@@ -36,6 +50,7 @@ function getInverses (a, n) {
   return (vec2.x % nn + nn) % nn;
 }
 
+
 /*
 ================
 Class Definition
@@ -43,10 +58,15 @@ Class Definition
 */
 
 // Note:
-// The implementation of ReversibleRandom class uses the type 'BigInt'
+// The implementation of ReversibleRandom class uses the type 'BigInt',
 // which may not be supported in early JS versions.
 // However, the type of the return value is 'Number'
 // so it's safe to directly do comparison, calculation and so on with the return value.
+// ----------
+// Note:
+// If you manually provide inv_a, make sure IT IS the inverses of a (mod m).
+// Also make sure a and m are coprime numbers.
+// Otherwise, there will not be a certain inv_a, thus this RNG cannot work properly.
 class ReversibleRandom {
   constructor (a, c, m, inv_a = undefined) {
     this.a = BigInt(a);
@@ -57,10 +77,16 @@ class ReversibleRandom {
       this.inv_a = BigInt(inv_a);
       console.assert(
         this.a * this.inv_a % this.m == 1,
-        'Error: the provided inv_a is not the inverses of a!'
+        'Error: the provided inv_a is not the inverses of a (mod m)!'
       );
     }
-    else this.inv_a = getInverses(a, m);
+    else {
+      this.inv_a = getInverses(a, m);
+      console.assert(
+        this.a * this.inv_a % this.m == 1,
+        'Error: the provided a and m are not coprime!'
+      );
+    }
     
     this.cur = BigInt(Math.floor(Math.random() * m)) % this.m;
   }
